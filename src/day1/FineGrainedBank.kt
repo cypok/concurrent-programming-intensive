b5346a1440b1e71803b3f53f042ea396869db0ec
Vladimir Parfinenko <vladimir.parfinenko@jetbrains.com>
2023-08-07 11:22:25 +0200

    fine-grained prettified

--- src/day1/FineGrainedBank.kt
+++ src/day1/FineGrainedBank.kt
@@ -8,24 +8,32 @@ import java.util.concurrent.locks.*
 class FineGrainedBank(accountsNumber: Int) : Bank {
     private val accounts: Array<Account> = Array(accountsNumber) { Account() }
 
-    private fun <T> withLocked(account: Account, action: () -> T): T {
+    private fun <T> withLocked(accId: Int, action: (Account) -> T): T {
+        val account = accounts[accId]
         account.lock.lock()
         try {
-            return action()
+            return action(account)
         } finally {
             account.lock.unlock()
         }
     }
 
+    private fun <T> withLocked(accId1: Int, accId2: Int, action: (Account, Account) -> T): T {
+        val okOrder = accId1 <= accId2
+        return withLocked(if (okOrder) accId1 else accId2) { acc1 ->
+            withLocked(if (okOrder) accId2 else accId1) { acc2 ->
+                action(if (okOrder) acc1 else acc2, if (okOrder) acc2 else acc1)
+            }
+        }
+    }
+
     override fun getAmount(id: Int): Long {
-        val account = accounts[id]
-        return withLocked(account) { account.amount }
+        return withLocked(id) { it.amount }
     }
 
     override fun deposit(id: Int, amount: Long): Long {
         require(amount > 0) { "Invalid amount: $amount" }
-        val account = accounts[id]
-        return withLocked(account) {
+        return withLocked(id) { account ->
             check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
             account.amount += amount
             account.amount
@@ -34,8 +42,7 @@ class FineGrainedBank(accountsNumber: Int) : Bank {
 
     override fun withdraw(id: Int, amount: Long): Long {
         require(amount > 0) { "Invalid amount: $amount" }
-        val account = accounts[id]
-        return withLocked(account) {
+        return withLocked(id) { account ->
             check(account.amount - amount >= 0) { "Underflow" }
             account.amount -= amount
             account.amount
@@ -45,18 +52,12 @@ class FineGrainedBank(accountsNumber: Int) : Bank {
     override fun transfer(fromId: Int, toId: Int, amount: Long) {
         require(amount > 0) { "Invalid amount: $amount" }
         require(fromId != toId) { "fromId == toId" }
-        val from = accounts[fromId]
-        val to = accounts[toId]
-
-        // Strict locking order here:
-        val (first, second) = if (fromId < toId) Pair(from, to) else Pair(to, from)
-
-        return withLocked(first) { withLocked(second) {
+        return withLocked(fromId, toId) { from, to ->
             check(amount <= from.amount) { "Underflow" }
             check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
             from.amount -= amount
             to.amount += amount
-        }}
+        }
     }
 
     /**
